451e98457643062f9674899e87eed60bf1199507
Khyati Kumari <108214217+Khyatikumari@users.noreply.github.com>
2026-05-07 10:47:59 +0530

    Time: 286 ms (31.03%), Space: 37 MB (99.14%) - LeetHub

--- 3660-jump-game-ix/3660-jump-game-ix.py
+++ 3660-jump-game-ix/3660-jump-game-ix.py
@@ -1,32 +1,36 @@
+from typing import List
+
 class Solution:
     def maxValue(self, nums: List[int]) -> List[int]:
         n = len(nums)
-
-        pre = [0] * n
-        suf = [0] * n
-        res = [0] * n
-
-        # prefix max
-        pre[0] = nums[0]
+        if n == 0:
+            return []
+        
+        # prefix maximums
+        pref_max = [0] * n
+        pref_max[0] = nums[0]
         for i in range(1, n):
-            pre[i] = max(pre[i - 1], nums[i])
-
-        # suffix min
-        suf[-1] = nums[-1]
-        for i in range(n - 2, -1, -1):
-            suf[i] = min(suf[i + 1], nums[i])
-
-        res[-1] = pre[-1]
-
-        # build answer
-        for i in range(n - 2, -1, -1):
-
-            # merge segment
-            if pre[i] > suf[i + 1]:
-                res[i] = res[i + 1]
-
-            # new segment
-            else:
-                res[i] = pre[i]
-
-        return res    
+            pref_max[i] = max(pref_max[i-1], nums[i])
+        
+        # suffix minimums
+        suff_min = [0] * n
+        suff_min[-1] = nums[-1]
+        for i in range(n-2, -1, -1):
+            suff_min[i] = min(suff_min[i+1], nums[i])
+        
+        ans = [0] * n
+        start = 0
+        for i in range(n-1):
+            if pref_max[i] <= suff_min[i+1]:
+                # segment [start, i]
+                seg_max = max(nums[start:i+1])
+                for j in range(start, i+1):
+                    ans[j] = seg_max
+                start = i + 1
+        
+        # last segment
+        seg_max = max(nums[start:])
+        for j in range(start, n):
+            ans[j] = seg_max
+        
+        return ans
