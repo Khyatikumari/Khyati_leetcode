d58d183be2da224f19fa6a7273d0946ac7526e98
Khyati Kumari <108214217+Khyatikumari@users.noreply.github.com>
2026-05-19 10:13:39 +0530

    Time: 0 ms (100%), Space: 37.9 MB (51.94%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.py
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -1,21 +1,16 @@
-from collections import deque
-
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        # Cast the sorted arrays into double-ended queues
-        queue1 = deque(nums1)
-        queue2 = deque(nums2)
-        
-        # Erode the streams from the front until a match or exhaustion
-        while queue1 and queue2:
-            head1 = queue1[0]
-            head2 = queue2[0]
-            
-            if head1 == head2:
-                return head1  # First mutual element encountered is guaranteed the minimum
-            elif head1 < head2:
-                queue1.popleft()  # Erode the smaller value
+        if nums1[-1] < nums2[0]:
+            return -1
+        if nums2[-1] < nums1[0]:
+            return -1
+        p1, p2 = 0, 0
+        while p1 < len(nums1) and p2 < len(nums2):
+            if nums1[p1] == nums2[p2]:
+                return nums1[p1]
+            elif nums1[p1] < nums2[p2]:
+                p1 += 1
             else:
-                queue2.popleft()  # Erode the smaller value
-                
-        return -1     
+                p2 += 1
+        
+        return -1
