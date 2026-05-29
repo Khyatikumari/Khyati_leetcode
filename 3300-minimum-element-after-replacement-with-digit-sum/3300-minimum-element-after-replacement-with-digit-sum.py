13d0cd39f674eb1870d33b0e304bb45a9068c8a9
Khyati Kumari <108214217+Khyatikumari@users.noreply.github.com>
2026-05-29 08:32:44 +0530

    Time: 4 ms (51.47%), Space: 19.3 MB (56.38%) - LeetHub

--- 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.py
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.py
@@ -1,14 +1,12 @@
 class Solution:
     def minElement(self, nums: List[int]) -> int:
-        min_val = float('inf')
         
-        for num in nums:
-            current_sum = 0
-            
-            while num > 0:
-                current_sum += num % 10
-                num //= 10
-            
-            min_val = min(min_val, current_sum)
-                
-        return min_val
+        min_res = float('inf')
+
+        for n in nums:
+            cur = 0
+            while n:
+                cur += n % 10
+                n //= 10
+            min_res = min(min_res, int(cur))
+        return min_res
