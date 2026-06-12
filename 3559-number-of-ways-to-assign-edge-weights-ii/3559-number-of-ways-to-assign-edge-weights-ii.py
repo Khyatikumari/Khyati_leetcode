e5a04f3b4691deca156450dc9e5d267fae434f75
Khyati Kumari <108214217+Khyatikumari@users.noreply.github.com>
2026-06-12 21:48:18 +0530

    Time: 747 ms (92.45%), Space: 178 MB (47.17%) - LeetHub

--- 3559-number-of-ways-to-assign-edge-weights-ii/3559-number-of-ways-to-assign-edge-weights-ii.py
+++ 3559-number-of-ways-to-assign-edge-weights-ii/3559-number-of-ways-to-assign-edge-weights-ii.py
@@ -1,79 +1,68 @@
+MX = 100001
+mod = 10**9+7
+fac = [1]*MX
+for i in range(1,MX):
+    fac[i] = fac[i-1]*(i)%mod
+inv = [1]*MX
+inv[-1] = pow(fac[-1],mod-2,mod)
+for i in range(MX-2,-1,-1):
+    inv[i] = inv[i+1]*(i+1)%mod
 class Solution:
-    MOD = 10**9 + 7
-
-    def modPow(self, a, b):
-        ans = 1
-
-        while b > 0:
-            if b & 1:
-                ans = (ans * a) % self.MOD
-
-            a = (a * a) % self.MOD
-            b >>= 1
-
-        return ans
-
-    def dfs(self, node, parent):
-        self.up[node][0] = parent
-
-        for j in range(1, self.LOG):
-            self.up[node][j] = self.up[self.up[node][j - 1]][j - 1]
-
-        for neighbour in self.adj[node]:
-            if neighbour == parent:
-                continue
-
-            self.depth[neighbour] = self.depth[node] + 1
-            self.dfs(neighbour, node)
-
-    def lca(self, u, v):
-        if self.depth[u] < self.depth[v]:
-            u, v = v, u
-
-        diff = self.depth[u] - self.depth[v]
-
-        for j in range(self.LOG - 1, -1, -1):
-            if diff & (1 << j):
-                u = self.up[u][j]
-
-        if u == v:
-            return u
-
-        for j in range(self.LOG - 1, -1, -1):
-            if self.up[u][j] != self.up[v][j]:
-                u = self.up[u][j]
-                v = self.up[v][j]
-
-        return self.up[u][0]
-
     def assignEdgeWeights(self, edges: List[List[int]], queries: List[List[int]]) -> List[int]:
-        n = len(edges) + 1
-
-        self.LOG = 1
-        while (1 << self.LOG) <= n:
-            self.LOG += 1
-
-        self.adj = [[] for _ in range(n + 1)]
-
-        for u, v in edges:
-            self.adj[u].append(v)
-            self.adj[v].append(u)
-
-        self.depth = [0] * (n + 1)
-        self.up = [[0] * self.LOG for _ in range(n + 1)]
-
-        self.dfs(1, 0)
-
-        ans = []
-
-        for u, v in queries:
-            L = self.lca(u, v)
-
-            dist = self.depth[u] + self.depth[v] - 2 * self.depth[L]
-
-            if dist == 0:
-                ans.append(0)
+        n = len(edges)+1
+        deep = [0]*(n+1)
+        g = [[] for _ in range(n+1)]
+        q = [[] for _ in range(n+1)]
+        ans = [0]*(len(queries))
+        vis = [False] * (n+1)
+        father = [i for i in range(n+1)]
+        for u,v in edges:
+            g[u].append(v)
+            g[v].append(u)
+        for idx,[u,v] in enumerate(queries):
+            q[u].append((v,idx))
+            q[v].append((u,idx))
+        
+        def find(x):
+            while father[x]!=x:
+                father[x] = father[father[x]]
+                x = father[x]
+            return father[x]
+        
+        def tarjan(u,parent):
+            vis[u] = True
+            for v in g[u]:
+                if v == parent:
+                    continue
+                tarjan(v,u)
+                father[v] = u
+            
+            for node,idx in q[u]:
+                if vis[node]:
+                    ans[idx] = find(node)
+        tarjan(1,0)
+        def f(u,fa,d):
+            nonlocal deep
+            deep[u] = d
+            for v in g[u]:
+                if v==fa:continue
+                f(v,u,d+1)
+        f(1,0,0)
+        res = [0]*len(queries)
+        def comb(n,a):
+            return fac[n]*inv[a]*inv[n-a]%mod
+        def f1(x):
+            ret = 0
+            for i in range(1,x+1,2):
+                ret = (ret + comb(x,i))%mod
+            return ret
+        for i,[a,b] in enumerate(queries):
+            lca = ans[i]
+            x = deep[a]+deep[b] - 2*deep[lca]
+            if x == 0:
+                res[i] = 0
             else:
-                ans.append(self.modPow(2, dist - 1))
-
-        return ans
+                res[i] = pow(2, x-1, mod)
+        return res
+        
+        
