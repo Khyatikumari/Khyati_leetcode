10af89088adb5e1004cfba1a9c4a0ac82ac57e1b
Khyati Kumari <108214217+Khyatikumari@users.noreply.github.com>
2026-07-10 13:28:15 +0530

    Time: 121 ms (94.74%), Space: 292.4 MB (72.37%) - LeetHub

--- 3534-path-existence-queries-in-a-graph-ii/3534-path-existence-queries-in-a-graph-ii.cpp
+++ 3534-path-existence-queries-in-a-graph-ii/3534-path-existence-queries-in-a-graph-ii.cpp
@@ -1,43 +1,57 @@
+static int st[100001][18] = {0};
 class Solution {
 public:
     vector<int> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        vector<pair<int, int>> newNums(n);
-        for (int i = 0; i < n; i++)
-            newNums[i] = {nums[i], i};
-        sort(newNums.begin(), newNums.end());
+        vector<pair<int, int>> vec;
+        for (int i = 0; i < n; ++i) {
+            vec.push_back({nums[i], i});
+        }
+        sort(vec.begin(), vec.end());
 
-        vector<int> getI(n);
-        for (int i = 0; i < n; i++)
-            getI[newNums[i].second] = i;
-
-        vector<vector<int>> st(n, vector<int>(18));
-
-        int r = 0;
-        for (int i = 0; i < n; i++) {
-            if (r < i) r = i;
-            while (r + 1 < n &&
-                   newNums[r + 1].first - newNums[r].first <= maxDiff &&
-                   newNums[r + 1].first - newNums[i].first <= maxDiff)
-                r++;
-            st[i][0] = r;
+        vector<int> getSortIdx(n);
+        for (int i = 0; i < n; ++i) {
+            getSortIdx[vec[i].second] = i;
         }
 
-        for (int j = 1; j < 18; j++)
-            for (int i = 0; i < n; i++)
+        // vector<vector<int>> st(n, vector<int>(18, n - 1));
+        int l = 0;
+        for (int r = 0; r < n; ++r) {
+            while (vec[r].first - vec[l].first > maxDiff) {
+                st[l][0] = r - 1;
+                ++l;
+            }
+        }
+        while (l < n) {
+            st[l][0] = n - 1;
+            l++;
+        }
+        for (int j = 1; j < 18; ++j) {
+            for (int i = 0; i < n; ++i) {
                 st[i][j] = st[st[i][j - 1]][j - 1];
-
-        vector<int> ans(queries.size(), -1);
-        for (int i = 0; i < (int)queries.size(); i++) {
-            int a = getI[queries[i][0]];
-            int b = getI[queries[i][1]];
+            }
+        }
+        int m = queries.size();
+        vector<int> ans(m, -1);
+        for (int i = 0; i < m; ++i) {
+            int a = getSortIdx[queries[i][0]];
+            int b = getSortIdx[queries[i][1]];
             if (a > b) swap(a, b);
-            if (a == b) { ans[i] = 0; continue; }
+            if (a == b) {
+                ans[i] = 0;
+                continue;
+            }
 
-            int curr = a, steps = 0;
-            for (int j = 17; j >= 0; j--)
-                if (st[curr][j] < b) { curr = st[curr][j]; steps += (1 << j); }
-
-            ans[i] = (st[curr][0] >= b) ? steps + 1 : -1;
+            int cur = a;
+            int step = 0;
+            for (int j = 17; j >= 0; --j) {
+                if (st[cur][j] < b) {
+                    step += (1 << j);
+                    cur = st[cur][j];
+                }
+            }
+            if (st[cur][0] >= b) {
+                ans[i] = step + 1;
+            }
         }
         return ans;
     }
