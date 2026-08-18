2980e9b953f8dd2d5702fbc3be30ccd659532065
Khyati Kumari <108214217+Khyatikumari@users.noreply.github.com>
2026-08-18 22:48:11 +0530

    Time: 0 ms (100%), Space: 29.1 MB (73.68%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -3,46 +3,36 @@ public:
     int largestInteger(vector<int>& nums, int k) {
         int n = nums.size();
 
-        unordered_map<int, int> mp;
+        vector<int> freq(51, 0);
 
-        for (int i = 0; i < n; i++) {
-            ++mp[nums[i]];
+        for (int x : nums) {
+            freq[x]++;
         }
-
-        if (k == nums.size()) {
+        if (k == n) {
             return *max_element(nums.begin(), nums.end());
         }
-
         if (k == 1) {
-            int maxValue = -1;
+            int ans = -1;
 
-            for (int i = 0; i < n; i++) {
-                if (mp[nums[i]] == 1 && nums[i] > maxValue) {
-                    maxValue = nums[i];
+            for (int x : nums) {
+                if (freq[x] == 1) {
+                    ans = max(ans, x);
                 }
             }
 
-            return maxValue;
+            return ans;
         }
 
-        n = n - 1;
+        int ans = -1;
 
-        if (nums[0] == nums[n]) {
-            return -1;
+        if (freq[nums[0]] == 1) {
+            ans = max(ans, nums[0]);
         }
 
-        if (mp[nums[0]] == 1 && mp[nums[n]] == 1) {
-            return max(nums[0], nums[n]);
+        if (freq[nums[n - 1]] == 1) {
+            ans = max(ans, nums[n - 1]);
         }
 
-        if (mp[nums[0]] == 1 && mp[nums[n]] > 1) {
-            return nums[0];
-        }
-
-        if (mp[nums[n]] == 1 && mp[nums[0]] > 1) {
-            return nums[n];
-        }
-
-        return -1;
+        return ans;
     }
 };
