97f7db3fd263ef6c0db08e42ba5714145b8ea62e
Khyati Kumari <108214217+Khyatikumari@users.noreply.github.com>
2026-09-18 11:01:37 +0530

    Time: 47 ms (96.46%), Space: 20.3 MB (70.8%) - LeetHub

--- 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.py
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.py
@@ -1,59 +1,26 @@
 class Solution:
-    def maxNumOfSubstrings(self, s):
+    def maxNumOfSubstrings(self, s: str) -> List[str]:
         n = len(s)
+        counts = Counter(s)
+        first = {k: s.find(k) for k in counts}
+        last = {k: s.rfind(k) for k in counts}
+        
+        res = []
+        queue = deque()
 
-        first = [n] * 26
-        last = [-1] * 26
+        for k in counts:
+            queue.appendleft([first[k], last[k], counts[k]])
+            left, right, total = inf, -inf, 0
 
-        # Find first and last occurrence of every character
-        for i in range(n):
-            ch = ord(s[i]) - ord('a')
-
-            if first[ch] == n:
-                first[ch] = i
-
-            last[ch] = i
-
-        intervals = []
-
-        # Build valid intervals
-        for ch in range(26):
-            if last[ch] == -1:
-                continue
-
-            start = first[ch]
-            end = last[ch]
-
-            valid = True
-            i = start
-
-            # Keep checking even when end gets expanded
-            while i <= end:
-                current = ord(s[i]) - ord('a')
-
-                # This character has an occurrence before start
-                if first[current] < start:
-                    valid = False
+            for x, y, z in queue:
+                total += z
+                left = min(left, x)
+                right = max(right, y)
+                if total == right - left + 1:
                     break
 
-                # Include all occurrences of this character
-                end = max(end, last[current])
+            if total == right - left + 1:
+                res.append(s[left:right+1])
+                queue = deque()
 
-                i += 1
-
-            if valid:
-                intervals.append((start, end))
-
-        # Sort by ending position
-        intervals.sort(key=lambda x: (x[1], x[1] - x[0]))
-
-        answer = []
-        previous_end = -1
-
-        # Greedily choose non-overlapping intervals
-        for start, end in intervals:
-            if start > previous_end:
-                answer.append(s[start:end + 1])
-                previous_end = end
-
-        return answer
+        return res
